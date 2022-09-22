4d881ce04cde52d40cd7f5d134add4b5af967ce0
Eyad-Ziad <114187648+Eyad-Ziad@users.noreply.github.com>
2022-09-22 22:49:44 +0300

    Update app.py

--- Assignment_1/python/app.py
+++ Assignment_1/python/app.py
@@ -1,3 +1,11 @@
+
+class person: 
+    def __init__(self,id,name,age,weight):
+        self.id = id
+        self.name = name
+        self.age = age
+        self.weight = weight
+        
 with open("./data/Data.csv", "r") as file:
     raw_string = file.read()
 
